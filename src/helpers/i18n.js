5595455b42b5a5ce7f2c589d81e31ae349b5d869
Rich McNeary <rlmcneary2@gmail.com>
2016-08-11 18:02:12 -0400

    Allow the HTML to specify the index of the locale to use.

--- src/helpers/i18n.js
+++ src/helpers/i18n.js
@@ -13,35 +13,30 @@ let strings;
  * file; for example {{#i18n}}header-title{{/i18n}}.
  * @module
  */
-module.exports = options => {
-    // To set the locale use the command "npm run build --locale=en-US",
-    // obviously replacing "en-US" with the locale desired.
-
-    // The current locale to build with is passed in
-    // set by src/data/ka.js
-    let fileName = options.data.root.ka.locale;
-
-    // The strings for a specific language will be stored globally once per
-    // session.
-    if (!strings) {
-        strings = require(`../i18n/${fileName}.json`);
-
-        // There might not be strings for a specific locale, but rather a more
-        // generic language.
-        if (!strings) {
-            fileName = options.data.root.ka.language;
-            strings = require(`../i18n/${fileName}.json`);
-        }
-    }
-
-    const key = options.fn(this);
-
+module.exports = function () { // Babel bug will incorrectly transform "arguments" in an arrow function into "_arguments".
     let text;
-    if (strings) {
-        text = strings[key] || `-&gt;${key}&lt;-`;
-    } else {
-        text = `LOCALE MISSING - '${fileName}'`;
+    try {
+        // The current locale to build with is passed in
+        // set by src/data/ka.js
+        const options = arguments[arguments.length - 1];
+        let stringsFileName = options.data.root.ka.locales[0];
+
+        // The strings for a specific language will be stored globally once per
+        // session.
+        if (!strings) {
+            strings = require(`../i18n/${stringsFileName}.json`);
+        }
+
+        const key = options.fn(this);
+
+        if (strings) {
+            text = strings[key] || `-&gt;${key}&lt;-`;
+        } else {
+            text = `LOCALE MISSING - '${stringsFileName}'`;
+        }
+    } catch (err) {
+        console.log(`i18n.js - ${err.message || JSON.stringify(err)}`);
     }
 
-    return `<span>${text}</span>`;
+    return text;
 };
