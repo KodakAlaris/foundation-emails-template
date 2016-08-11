3a29e025ac1cd5871f4c9f36ab9c3883965d39b2
Rich McNeary <rlmcneary2@gmail.com>
2016-08-11 14:40:13 -0400

    Read all the pages from src/pages and make them available to Panini data. File naming function to add the locale as a suffix to the file name.

--- src/data/ka.js
+++ src/data/ka.js
@@ -1,8 +1,12 @@
 "use strict";
 
 
-const data = { locale: process.env.npm_config_locale || "en-US" };
+const fs = require("fs");
+const path = require("path");
 
+
+// Locale information provided via command line or the default.
+const data = { locale: process.env.npm_config_locale || "en-US" };
 
 if (-1 < data.locale.indexOf("-")) {
     const [language, region] = data.locale.split("-");
@@ -13,4 +17,40 @@ if (-1 < data.locale.indexOf("-")) {
 }
 
 
+// This is the one tru elocation for generating file names that include a
+// locale.
+data.createLocalePageName = createLocalePageName;
+
+
+// Get all the file names that will be created. Used by index.html to generate
+// links to all of the pages.
+data.pages = fs.readdirSync("src/pages")
+    .filter(item => {
+        const {name} = path.parse(item);
+        return name !== "index";
+    })
+    .map(item => {
+        return createLocalePageName(item);
+    });
+
+
 module.exports = data;
+
+
+function createLocalePageName(fileName) {
+    // Sometimes we'll just get a string that's a path.
+    if (typeof fileName === "string") {
+        const {ext, name} = path.parse(fileName);
+        return `${name}_${data.locale}${ext}`;
+    }
+
+    // Other times an object that's either an fs pathObject or a gulp-rename
+    // file information object (not quite the same thing).
+    if (fileName.hasOwnProperty("basename")) { // gulp-rename
+        fileName.basename += `_${data.locale}`;
+    } else if (fileName.hasOwnProperty("base")) { // fs pathObject
+        fileName.base += `_${data.locale}`;
+    }
+
+    return fileName;
+}
