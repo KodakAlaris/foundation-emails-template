47299519cdd89aadf44f39517e931ec1d5723775
Rich McNeary <rlmcneary2@gmail.com>
2016-08-11 12:17:13 -0400

    Added comments about how the i18n and ifregion helpers are to be used.

--- src/helpers/i18n.js
+++ src/helpers/i18n.js
@@ -4,6 +4,15 @@
 let strings;
 
 
+/**
+ * i18n is a handlebars block helper that returns a localized string. The
+ * locale is determined when the application starts by passing a --locale
+ * parameter; for example "npm build --locale=en-US". In this case pages with
+ * US English strings will be rendered. To use in an HTML page or partial
+ * include the nugget {{#i18n}} along with a key that exists in an i18n JSON
+ * file; for example {{#i18n}}header-title{{/i18n}}.
+ * @module
+ */
 module.exports = options => {
     // To set the locale use the command "npm run build --locale=en-US",
     // obviously replacing "en-US" with the locale desired.
--- src/helpers/ifregion.js
+++ src/helpers/ifregion.js
@@ -4,6 +4,18 @@
 let commandLineRegion;
 
 
+/**
+ * This is a handlebars conditional helper that returns true if the region
+ * parameter matches the runtime region. The region is determined when the
+ * application starts by passing a --locale parameter; for example
+ * "npm build --locale=en-US". ifregion can be used to provide different pages,
+ * partials, or HTML based on the region. To use in an HTML page or partial
+ * include the nugget {{#ifregion <region>}} and a region code; for example
+ * {{#ifregion "us"}}. Since this is a conditional it can be used with built-in
+ * handlebars conditionals such as {{else}}.
+ * @module
+ * @parameter {string} region The region that will be tested to see if it matches the runtime region. The case of the string is ignored.
+ */
 module.exports = (region, options) => {
     // To set the locale use the command "npm run build --locale=en-US",
     // obviously replacing "en-US" with the locale desired.
